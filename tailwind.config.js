d41a2d7be0faa4360b3bb18388f67394816706c4
Ankur <am8468616@gmail.com>
2025-10-10 18:22:46 +0530

    nativewind and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  presets: [ require("nativewind/preset") ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#1E40AF",
+        secondary: "#FBBF24",
+        light: {
+          100: "#D6C6EF",
+          200: "#ABB5DB",
+          300: "#9CA5AB",
+          400: "#9CA3AF",
+          500: "#6B7280",
+        },
+        dark:{
+          100: "#221f3d",
+          200: "#0f0d23",
+          300: "#111827",
+          400: "#0F172A",
+          500: "#0B1120",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
