f598a04caf53220e1c40d4a6236fc485c11465c9
Ankur <am8468616@gmail.com>
2025-10-11 15:25:42 +0530

    routing setup with resources addition

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const search = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Search</Text>
+    </View>
+  );
+};
+
+export default search;
+
+const styles = StyleSheet.create({
+  container: {}
+});
---
+++ app/movies/[id].tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const MovieDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MovieDetails
+      </Text>
+    </View>
+  );
+};
+
+export default MovieDetails
+;
+
+const styles = StyleSheet.create({
+  container: {}
+});
