4b0dee7f20f06abcf9ac6677ea459eae0942125f
Ankur <am8468616@gmail.com>
2025-10-16 00:30:10 +0530

    hide status bar from the app

--- demoMovieApp/app/_layout.tsx
+++ demoMovieApp/app/_layout.tsx
@@ -1,14 +1,16 @@
 import { Stack } from "expo-router";
 import './globals.css'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
+    <>
+        <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
       </Stack>
-    </SafeAreaProvider>
+    </>
   );
 }
