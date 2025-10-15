dac2414d5babc0a73c200d36acb6eebe20487a01
Ankur <am8468616@gmail.com>
2025-10-16 00:25:38 +0530

    trending card created and inserted in home page

--- demoMovieApp/app/(tabs)/index.tsx
+++ demoMovieApp/app/(tabs)/index.tsx
@@ -14,8 +14,15 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 export default function Index() {
   const router = useRouter();
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
   const {
     data: movies,
     loading: moviesLoading,
@@ -42,31 +49,44 @@ export default function Index() {
           className="w-12 h-10 mt-20 mb-5 mx-auto"
           style={styles.logo}
         />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search movie"
             />
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{ gap: 25 }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                />
+              </View>
+            )}
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
               <FlatList
                 data={movies}
-                renderItem={({ item }) => (
-                  <MovieCard
-                   {...item}
-                  />
-                )}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id}
                 numColumns={3}
                 columnWrapperStyle={{
--- demoMovieApp/app/(tabs)/search.tsx
+++ demoMovieApp/app/(tabs)/search.tsx
@@ -33,7 +33,7 @@ const Search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
-        if(movies?.length>0 && movies?.[0]) await updateSearchCount(searchQuery,movies[0]);
+        
       } else {
         reset();
       }
@@ -41,6 +41,10 @@ const Search = () => {
     return () => clearTimeout(timeoutId);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchQuery]);
+
+  useEffect(() => {
+   if(movies?.length>0 && movies?.[0]) updateSearchCount(searchQuery,movies[0]);
+  },[movies]);
 
   return (
     <View className="flex-1 bg-primary">
